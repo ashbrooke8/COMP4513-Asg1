fcc1b5e5547deb6f288d85c928a06a270f4b6c45
ashbrooke8 <ashleybhunchak@gmail.com>
2025-02-08 16:54:59 -0700

    made some of the basic api routes

--- art-api.js
+++ art-api.js
@@ -1,120 +1,25 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const supa = require("@supabase/supabase-js");
-const supaUrl = process.env.SUPABASE_URL;
-const supaAnonKey = process.env.SUPABASE_ANON_KEY;
-const supabase = supa.createClient(supaUrl, supaAnonKey);
+// const supa = require("@supabase/supabase-js");
+// const supaUrl = process.env.SUPABASE_URL;
+// const supaAnonKey = process.env.SUPABASE_ANON_KEY;
+// const supabase = supa.createClient(supaUrl, supaAnonKey);
 
 const router = require("./scripts/art-router.js");
 
-// router.handleEntireTable(app, "eras");
-// router.handleEntireTable(app, "galleries");
-// router.handleEntireTable(app, "artists");
-// router.handleEntireTable(app, "paintings");
-// router.handleEntireTable(app, "genres");
-//base table routes
-const tables = ["eras", "galleries", "artists", "paintings", "genres"];
+//base table routes to return all fields in table
+const tables = ["eras", "galleries", "artists"];
 tables.forEach((table) => {
   router.handleEntireTable(app, table);
 });
 
-// /api/galleries/30 // /api/galleries/Calgary is this meant to be for the Id check?
-app.get("/api/galleries/:value", async (req, res) => {
-  const table = req.params.table;
-  const value = req.params.value;
-  const { data, error } = await supabase
-    .from("galleries")
-    .select("*")
-    .eq("galleryId", value);
-  if (error) {
-    console.error("Error fetching data: ", error);
-    return res.status(500).json({ error: "server error" });
-  }
-  if (data.length === 0) {
-    console.error("No content found");
-    return res.status(404).json({ message: "No data found" });
-  }
-  res.send(data);
-});
-
-// /api/galleries/country/fra
-app.get("/api/galleries/country/:value", async (req, res) => {
-  const table = req.params.table;
-  const value = req.params.value;
-  const { data, error } = await supabase
-    .from("galleries")
-    .select("*")
-    .ilike("galleryCountry", `%${value}%`);
-  if (error) {
-    console.error("Error fetching data: ", error);
-    return res.status(500).json({ error: "server error" });
-  }
-  if (data.length === 0) {
-    console.error("No content found");
-    return res.status(404).json({ message: "No data found" });
-  }
-  res.send(data);
-});
-
-// /api/artists/12
-// /api/artists/1223423
-app.get("/api/artists/:value", async (req, res) => {
-  const value = req.params.value;
-  const { data, error } = await supabase
-    .from("artists")
-    .select("*")
-    .eq("artistId", value);
-  if (error) {
-    console.error("Error fetching data: ", error);
-    return res.status(500).json({ error: "server error" });
-  }
-  if (data.length === 0) {
-    console.error("No content found");
-    return res.status(404).json({ message: "No data found" });
-  }
-  res.send(data);
-});
-
-// /api/artists/search/ma
-// /api/artists/search/mA
-app.get("/api/artists/search/:value", async (req, res) => {
-  const value = req.params.value;
-  const { data, error } = await supabase
-    .from("artists")
-    .select("*")
-    .ilike("firstName", `%${value}%`);
-  if (error) {
-    console.error("Error fetching data: ", error);
-    return res.status(500).json({ error: "server error" });
-  }
-  if (data.length === 0) {
-    console.error("No content found");
-    return res.status(404).json({ message: "No data found" });
-  }
-  res.send(data);
-});
-
-// /api/artists/country/fra
-// /api/paintings/sort/year
-// /api/paintings/63
-// /api/paintings/search/port
-// /api/paintings/search/pORt
-// /api/paintings/search/connolly
-// /api/paintings/1800/1850
-// /api/paintings/galleries/5
-// /api/paintings/artist/16
-// /api/paintings/artist/666
-// /api/paintings/artist/country/ital
-// /api/genres/76
-// /api/genres/painting/408
-// /api/genres/painting/jsdfhg
-// /api/paintings/genre/78
-// /api/paintings/era/2
-// /api/counts/genres
-// /api/counts/artists
-// /api/counts/topgenres/20
-// /api/counts/topgenres/2034958
+router.handleAllPaintings(app);
+router.handleAllGenres(app);
+router.handleSpecificResult(app, "galleries", "galleryId");
+router.handleSpecificResult(app, "artists", "artistId");
+router.handleSpecificPainting(app);
+router.handleSpecificResult(app, "genres", "genreId");
 
 app.listen(8080, () => {
   console.log("Server listening on port 8080");
--- scripts/art-router.js
+++ scripts/art-router.js
@@ -4,19 +4,93 @@ const supaUrl = process.env.SUPABASE_URL;
 const supaAnonKey = process.env.SUPABASE_ANON_KEY;
 const supabase = supa.createClient(supaUrl, supaAnonKey);
 
+const handleError = (data, error) => {
+  if (error) {
+    console.error("Error fetching data: ", error);
+    return res.status(500).json({ error: "server error" });
+  }
+  if (data.length === 0) {
+    console.error("No content found");
+    return res.status(404).json({ message: "No data found" });
+  }
+};
+
 const handleEntireTable = (app, table) => {
   app.get(`/api/${table}`, async (req, res) => {
     const { data, error } = await supabase.from(table).select("*");
-    if (error) {
-      console.error("Error fetching data: ", error);
-      return res.status(500).json({ error: "server error" });
-    }
-    if (data.length === 0) {
-      console.error("No content found");
-      return res.status(404).json({ message: "No data found" });
-    }
+    handleError(data, error);
     res.send(data);
   });
 };
 
-module.exports = { supabase, handleEntireTable };
+const handleAllPaintings = (app) => {
+  app.get("/api/paintings", async (req, res) => {
+    const { data, error } = await supabase
+      .from("paintings")
+      .select(
+        `paintingId, artistId (artistId, firstName, lastName, nationality, gender, yearOfBirth, yearOfDeath, details, artistLink), galleryId (galleryId, galleryName, galleryNativeName, galleryCity, galleryAddress, galleryCountry, latitude, longitude, galleryWebSite, flickrPlaceId, yahooWoeId, googlePlaceId), imageFileName, title, shapeId (shapeName), museumLink, accessionNumber, copyrightText, description, excerpt, yearOfWork, width, height, medium, cost, MSRP, googleLink, googleDescription, wikiLink, jsonAnnotations`
+      )
+      .order("title", { ascending: true });
+    handleError(data, error);
+    res.send(data);
+  });
+};
+
+const handleAllGenres = (app) => {
+  app.get("/api/genres", async (req, res) => {
+    const { data, error } = await supabase
+      .from("genres")
+      .select(
+        `genreId, genreName, eraId(eraId, eraName, eraYears), description, wikiLink`
+      );
+    handleError(data, error);
+    res.send(data);
+  });
+};
+
+const handleSpecificResult = (app, table, idField) => {
+  app.get(`/api/${table}/:ref`, async (req, res) => {
+    const { data, error } = await supabase
+      .from(table)
+      .select("*")
+      .eq(idField, req.params.ref);
+    handleError(data, error);
+    res.send(data);
+  });
+};
+
+const handleSpecificPainting = (app) => {
+  app.get("/api/paintings/:ref", async (req, res) => {
+    const { data, error } = await supabase
+      .from("paintings")
+      .select(
+        `paintingId, artistId (firstName, lastName, nationality, gender, yearOfBirth, yearOfDeath, details, artistLink), galleryId (galleryName, galleryNativeName, galleryCity, galleryAddress, galleryCountry, latitude, longitude, galleryWebSite, flickrPlaceId, yahooWoeId, googlePlaceId), imageFileName, title, shapeId (shapeName), museumLink, accessionNumber, copyrightText, description, excerpt, yearOfWork, width, height, medium, cost, MSRP, googleLink, googleDescription, wikiLink, jsonAnnotations`
+      )
+      .eq("paintingId", req.params.ref);
+    handleError(data, error);
+    res.send(data);
+  });
+};
+
+const handleSpecificGenre = (app) => {
+  app.get("/api/genres/:ref", async (req, res) => {
+    const { data, error } = await supabase
+      .from("genres")
+      .select(
+        `genreId, genreName, eraId(eraId, eraName, eraYears), description, wikiLink`
+      )
+      .eq("genreId", req.params.ref);
+    handleError(data, error);
+    res.send(data);
+  });
+};
+
+module.exports = {
+  supabase,
+  handleEntireTable,
+  handleAllPaintings,
+  handleAllGenres,
+  handleSpecificResult,
+  handleSpecificPainting,
+  handleSpecificGenre,
+};
